7f72c3afc8c6f4eb31ac6dbdf3d6db1e4a671321
mberndt92 <maximilian_berndt@icloud.com>
2023-04-01 08:33:04 +0900

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Maximilian Berndt on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
